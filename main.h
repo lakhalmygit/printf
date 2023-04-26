d52ef7bf498d54ed090170b4002b6ace438ab4c6
Ma95hdi <kamalmahdi040@gamil.com>
2023-04-26 05:22:15 -0700

    header file

--- main.h
+++ main.h
@@ -1,8 +1,40 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef _PRINTF_H
+#define _PRINTF_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+/**
+  * struct parameters - parameters struct
+  *
+  * @unsign: flag if unsigned value
+  *
+  * @plus_flag: on if plus_flag specified
+  * @space_flag: on if hashtag_flag specified
+  * @hashtag_flag: on if _flag specified
+  * @zero_flag: on if _flag specified
+  * @minus_flag: on if _flag specified
+  *
+  * @width: field specified
+  * @precision: field precision specified
+  *
+  * @h_modifier: on if h_modifier is specified
+  * @l_modifier: on if l_modifier is specified
+  *
+  */
 
 typedef struct formats
 {
