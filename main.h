afebe0fef6ac318e0263c93e8d04902b5c64b00d
ikram <ikramlakhal77@gmail.com>
2023-04-26 05:42:34 -0700

    by ikram

--- main.h
+++ main.h
@@ -1,49 +1,11 @@
 #ifndef _PRINTF_H
 #define _PRINTF_H
 
-<<<<<<< HEAD
-=======
 #include <stdarg.h>
->>>>>>> 9c22cc23729ba34fe2adc70a268ab46d0df07162
 #include <stdio.h>
 #include <unistd.h>
 #include <limits.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
-
-#define OUTPUT_BUF_SIZE 1024
-#define BUF_FLUSH -1
-
-#define NULL_STRING "(null)"
-
-#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-#define CONVERT_LOWERCASE 1
-#define CONVERT_UNSIGNED 2
-
-/**
-  * struct parameters - parameters struct
-  *
-  * @unsign: flag if unsigned value
-  *
-  * @plus_flag: on if plus_flag specified
-  * @space_flag: on if hashtag_flag specified
-  * @hashtag_flag: on if _flag specified
-  * @zero_flag: on if _flag specified
-  * @minus_flag: on if _flag specified
-  *
-  * @width: field specified
-  * @precision: field precision specified
-  *
-  * @h_modifier: on if h_modifier is specified
-  * @l_modifier: on if l_modifier is specified
-  *
-  */
-=======
->>>>>>> 9c22cc23729ba34fe2adc70a268ab46d0df07162
 
 #define OUTPUT_BUF_SIZE 1024
 #define BUF_FLUSH -1
