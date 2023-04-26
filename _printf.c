b09454bcb294c4147fa41fdad86e4f66b9a45337
Ma95hdi <kamalmahdi040@gamil.com>
2023-04-25 17:04:05 -0700

    printf file

--- _printf.c
+++ _printf.c
@@ -3,8 +3,8 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format.
+ * _printf - Prints anything
+ * @format: the format of string
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
